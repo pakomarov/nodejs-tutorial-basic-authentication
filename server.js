84f683a8a03d3c6b89894f735577f4c8391d647b
Pavel Komarov <komarov.pa.it@gmail.com>
2020-11-17 14:38:43 +0400

    Encrypted authentication using bcrypt

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 
 app.use(express.json());
 
@@ -9,13 +10,34 @@ app.get('/users', (req, res) => {
   res.json(users);
 });
 
-app.post('/users', (req, res) => {
-  const user = {
-    name: req.body.name,
-    password: req.body.password,
-  };
-  users.push(user);
-  res.status(201).send();
+app.post('/users', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = {
+      name: req.body.name,
+      password: hashedPassword,
+    };
+    users.push(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
 });
+
+app.post('/users/login', async (req, res) => {
+  const user = users.find(user => user.name === req.body.name);
+  if (user == null) {
+    return res.status(400).send('Cannot find user');
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success');
+    } else {
+      res.send('Not Allowed');
+    }
+  } catch {
+    res.status(500).send();
+  }
+})
 
 app.listen(3000);
